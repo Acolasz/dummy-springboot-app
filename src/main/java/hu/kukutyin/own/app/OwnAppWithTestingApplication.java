d3a3d0a56f45ff6782c2be2d2789f376c2a62c22
Ocskó Olivér <Oliver.Ocsko@dorsum.eu>
2023-02-05 16:30:59 +0100

    Initial commit

---
+++ src/main/java/hu/kukutyin/own/app/OwnAppWithTestingApplication.java
@@ -0,0 +1,13 @@
+package hu.kukutyin.own.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnAppWithTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnAppWithTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/kukutyin/own/app/OwnAppWithTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.kukutyin.own.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnAppWithTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
