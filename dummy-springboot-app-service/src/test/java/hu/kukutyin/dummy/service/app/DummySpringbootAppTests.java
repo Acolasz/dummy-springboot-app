8c9310317ee29dcf102c0288bcb35a5a431d400d
Ocskó Olivér <Oliver.Ocsko@dorsum.eu>
2023-02-25 21:06:46 +0100

    separate content dao and api

--- dummy-springboot-app-service/src/test/java/hu/kukutyin/dummy/service/app/DummySpringbootAppTests.java
+++ dummy-springboot-app-service/src/test/java/hu/kukutyin/dummy/service/app/DummySpringbootAppTests.java
@@ -8,6 +8,8 @@ import org.springframework.test.context.TestPropertySource;
 
 import hu.kukutyin.dummy.service.app.api.GreetingService;
 import hu.kukutyin.dummy.service.app.api.HealthCheckService;
+import hu.kukutyin.dummy.service.app.dao.ConstantsDao;
+import hu.kukutyin.dummy.service.app.dao.mapper.ConstantsMapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,9 +22,21 @@ class DummySpringbootAppTests {
     @Autowired
     private GreetingService greetingService;
 
+    @Autowired
+    private ConstantsDao constantsDao;
+
+    @Autowired
+    private ConstantsMapper constantsMapper;
+
     @Test
-    void contextLoads() {
+    void apiContextLoads() {
         assertThat(healthCheckService).isNotNull();
         assertThat(greetingService).isNotNull();
+    }
+
+    @Test
+    void daoContextLoads() {
+        assertThat(constantsDao).isNotNull();
+        assertThat(constantsMapper).isNotNull();
     }
 }
