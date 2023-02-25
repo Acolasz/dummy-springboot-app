c1a3a423aec8670c1d0d75049f06a71ee9f6eb1e
Ocskó Olivér <Oliver.Ocsko@dorsum.eu>
2023-02-26 00:42:09 +0100

    remove api service

--- dummy-springboot-app-service/src/test/java/hu/kukutyin/dummy/service/app/DummySpringbootAppTests.java
+++ dummy-springboot-app-service/src/test/java/hu/kukutyin/dummy/service/app/DummySpringbootAppTests.java
@@ -4,10 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-import hu.kukutyin.dummy.service.app.api.GreetingService;
-import hu.kukutyin.dummy.service.app.api.HealthCheckService;
 import hu.kukutyin.dummy.service.app.dao.ConstantsDao;
 import hu.kukutyin.dummy.service.app.dao.mapper.ConstantsMapper;
 
@@ -16,11 +13,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class DummySpringbootAppTests {
 
-    @Autowired
-    private HealthCheckService healthCheckService;
-
-    @Autowired
-    private GreetingService greetingService;
+//    @Autowired
+//    private HealthCheckService healthCheckService;
+//
+//    @Autowired
+//    private GreetingService greetingService;
 
     @Autowired
     private ConstantsDao constantsDao;
@@ -30,8 +27,8 @@ class DummySpringbootAppTests {
 
     @Test
     void apiContextLoads() {
-        assertThat(healthCheckService).isNotNull();
-        assertThat(greetingService).isNotNull();
+//        assertThat(healthCheckService).isNotNull();
+//        assertThat(greetingService).isNotNull();
     }
 
     @Test
