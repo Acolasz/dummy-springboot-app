96b189268b6edc6bf92849b6bb42464c11782255
Ocskó Olivér <Oliver.Ocsko@dorsum.eu>
2023-02-26 02:18:08 +0100

    binding swagger-ui with springfox

---
+++ dummy-springboot-app-service/src/main/java/hu/kukutyin/dummy/service/app/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package hu.kukutyin.dummy.service.app.config;
+
+import java.util.ArrayList;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+        private static final String BASE_PACKAGE_CONTROLLER = "hu.kukutyin.dummy.service";
+
+    private final BuildProperties buildProperties;
+
+    public SwaggerConfig(
+            BuildProperties buildProperties
+    ) {
+        this.buildProperties = buildProperties;
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE_CONTROLLER))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                buildProperties.getArtifact(),
+                String.format("%s %s", buildProperties.getArtifact(), "some custom description of API."),
+                buildProperties.getVersion(),
+                "Terms of service",
+                ApiInfo.DEFAULT_CONTACT,
+                "app.swaggerhub.com",
+                "https://app.swaggerhub.com/apis/Coinfirm-swagger/API/3.0.5#/",
+                new ArrayList()
+        );
+    }
+}
